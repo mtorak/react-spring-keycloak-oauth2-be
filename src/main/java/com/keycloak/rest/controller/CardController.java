b211269374f00e51beaf84ef471c5404fc304900
mtorak <mtorak2001@yahoo.com>
2025-04-16 16:32:23 +0300

    - keycloak be

---
+++ src/main/java/com/keycloak/rest/controller/CardController.java
@@ -0,0 +1,25 @@
+package com.keycloak.rest.controller;
+
+import com.keycloak.rest.entity.CardEntity;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CardController {
+
+    @PreAuthorize("hasAuthority('ROLE_view-cards')")
+    @RequestMapping("/cards")
+    public List<CardEntity> getCars(Authentication auth, Principal principal) {
+        return Arrays.asList(
+          new CardEntity("Audi", "white", 22000000L),
+          new CardEntity("BMW", "black", 24500000L),
+          new CardEntity("Mercedes", "silver", 45000000L));
+    }
+
+}
